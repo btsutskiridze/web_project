a7cae6e4b381df34d17294a5a63efdc7a8ec0c23
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-02 17:10:50 +0400

    feat: constructor and static method to establish connection with database

--- Modules/Services/DatabaseService.php
+++ Modules/Services/DatabaseService.php
@@ -3,4 +3,35 @@
 class DatabaseService
 {
 
+    private static ?DatabaseService $_instance = null;
+    private PDO $_pdo; //PHP Data Object
+    private $_query;
+    private bool $_error = false;
+    private $_results;
+    private int $_count = 0;
+
+    private function __construct()
+    {
+        try {
+            $this->_pdo = new PDO(
+                'mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/database'),
+                Config::get('mysql/username'),
+                Config::get('mysql/password')
+            );
+
+            echo 'Connected';
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+    }
+
+    public static function getInstance(): ?DatabaseService
+    {
+        if (!isset(self::$_instance)) {
+            self::$_instance = new DatabaseService();
+        }
+        return self::$_instance;
+    }
+
 }
