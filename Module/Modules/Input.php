07e32c5308574071e8c41bd98157f16203f45082
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-03 19:59:56 +0400

    feat: Input.php class that helps with checking inputs

--- Module/Modules/Input.php
+++ Module/Modules/Input.php
@@ -2,5 +2,19 @@
 
 class Input
 {
+    public static function exists($type = 'post'): bool
+    {
+        return match ($type) {
+            'post' => !empty($_POST),
+            'get' => !empty($_GET),
+            default => false,
+        };
+
+    }
+
+    public static function get($value): string
+    {
+        return $_POST[$value] ?? $_GET[$value] ?? '';
+    }
 
 }
