4423874a88664dbda1ff251455f78b9b7ce87d6e
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-05 17:15:28 +0400

    chore: edit rule

--- Module/Controllers/AuthController.php
+++ Module/Controllers/AuthController.php
@@ -20,13 +20,13 @@ class AuthController extends Controller
         $validation = $validator->check($_POST, [
             'username' => ['required' => true, 'min' => 2, 'max' => 20, 'unique' => 'users'],
             'email' => ['required' => true, 'min' => 2, 'max' => 60, 'unique' => 'users', 'email' => true],
-            'password' => ['required' => true, 'min' => 1,],
+            'password' => ['required' => true, 'min' => 6,],
             'confirm_password' => ['required' => true, 'matches' => 'password']
         ]);
 
 
         if ($validation->passed()) {
-            $user = (new User)->create([
+            (new User)->create([
                 'username' => Input::get('username'),
                 'email' => Input::get('email'),
                 'password' => Hash::make(Input::get('password')),
