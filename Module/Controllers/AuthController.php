16a6e414d349a89ae97567288ecef55a40aedee1
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-05 14:58:27 +0400

    feat: flashing success message

--- Module/Controllers/AuthController.php
+++ Module/Controllers/AuthController.php
@@ -4,29 +4,30 @@ class AuthController extends Controller
 {
     public function register(): void
     {
-        $errors = [];
 
-        if (Input::exists()) {
-            $validator = new Validator();
+        $validator = new Validator();
 
-            $validator->setAttributes([
-                'username' => 'Username',
-                'email' => 'Email',
-                'password' => 'Password',
-                'confirm_password' => 'Password Confirmation'
-            ]);
+        $validator->setAttributes([
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+            'confirm_password' => 'Password Confirmation'
+        ]);
 
-            $validation = $validator->check($_POST, [
-                'username' => ['required' => true, 'min' => 2, 'max' => 20, 'unique' => 'users'],
-                'email' => ['required' => true, 'min' => 2, 'max' => 60, 'unique' => 'users', 'email' => true],
-                'password' => ['required' => true, 'min' => 6,],
-                'confirm_password' => ['required' => true, 'matches' => 'password']
-            ]);
+        $validation = $validator->check($_POST, [
+            'username' => ['required' => true, 'min' => 2, 'max' => 20, 'unique' => 'users'],
+            'email' => ['required' => true, 'min' => 2, 'max' => 60, 'unique' => 'users', 'email' => true],
+            'password' => ['required' => true, 'min' => 1,],
+            'confirm_password' => ['required' => true, 'matches' => 'password']
+        ]);
 
-            $validation->passed() ? header('Location: login.php') : $errors = $validation->errors();
+
+        if ($validation->passed()) {
+            Session::flash('success', 'You registered successfully!');
+            header('location:/');
         }
 
-        $this->view('register', ['errors' => $errors]);
+        $this->view('register', ['errors' => $validation->errors()]);
     }
 
     public function login(): void
