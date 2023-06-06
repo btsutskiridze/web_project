becf97d81432ecde38fea38b628989e2174993f7
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-06 22:41:47 +0400

    feat: login and logout functionals in controllers

--- Module/Controllers/AuthController.php
+++ Module/Controllers/AuthController.php
@@ -2,6 +2,17 @@
 
 class AuthController extends Controller
 {
+    public function loginView(): void
+    {
+        $this->view('/login');
+    }
+
+    public function registerView(): void
+    {
+        $this->view('/register');
+    }
+
+
     /**
      * @throws Exception
      */
@@ -25,24 +36,53 @@ class AuthController extends Controller
         ]);
 
 
-        if ($validation->passed()) {
-            (new User)->create([
-                'username' => Input::get('username'),
-                'email' => Input::get('email'),
-                'password' => Hash::make(Input::get('password')),
-                'group' => 1,
-            ]);
-            Session::put('success', 'You registered successfully!');
-            Redirect::to('/register');
+        if (!$validation->passed()) {
+            $this->view('/register', ['errors' => $validation->errors()]);
+            return;
         }
 
+        (new User)->create([
+            'username' => Input::get('username'),
+            'email' => Input::get('email'),
+            'password' => Hash::make(Input::get('password')),
+            'group' => 1,
+        ]);
 
-        $this->view('/register', ['errors' => $validation->errors()]);
+        Session::put('message', 'You registered successfully!');
+        Redirect::to('/register');
     }
 
     public function login(): void
     {
+        $validator = new Validator();
 
+        $validator->setAttributes([
+            'email' => 'Email',
+            'password' => 'Password',
+        ]);
+
+
+        $validation = $validator->check($_POST, [
+            'email' => ['required' => true, 'min' => 2, 'max' => 60, 'email' => true],
+            'password' => ['required' => true, 'min' => 6,],
+        ]);
+
+        if (!$validation->passed()) {
+            $this->view('/login', ['errors' => $validation->errors()]);
+            return;
+        }
+
+        if ((new User)->login(Input::get('email'), Input::get('password'))) {
+            Redirect::to('/profile');
+            return;
+        }
+
+        $this->view('/login', ['errors' => ['password' => 'Wrong email or password']]);
     }
 
+    public function logout(): void
+    {
+        (new User)->logout();
+        Redirect::to('/login');
+    }
 }
---
+++ Module/Controllers/ProfileController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class ProfileController extends Controller
+{
+    public function index(): void
+    {
+        $user = new User;
+
+        $this->view('profile', ['user' => $user->data()]);
+    }
+}
