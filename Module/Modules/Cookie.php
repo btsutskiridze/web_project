2b65ca0d5afcfb8116e09aec242f207aa52821c2
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-06-07 13:35:19 +0400

    feat:cookie class

--- Module/Modules/Cookie.php
+++ Module/Modules/Cookie.php
@@ -2,5 +2,20 @@
 
 class Cookie
 {
+    public static function exists($name): bool
+    {
+        return (isset($_COOKIE[$name]));
+    }
+
+
+    public static function get($name): string
+    {
+        return $_COOKIE[$name];
+    }
+
+    public static function put($name, $value, $expiry): bool
+    {
+        return setcookie($name, $value, time() + $expiry, '/');
+    }
 
 }
